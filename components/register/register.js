5496c6878d1cd83762eba9bbe60bde8dbbcaf2e0
Diana <dianapauly16@hotmail.com>
2024-05-27 20:27:50 -0500

    feat: Inicio.html

--- components/register/register.js
+++ components/register/register.js
@@ -1,4 +1,4 @@
-document.getElementById('registerForm').addEventListener('submit', function(event) {
+document.getElementById('register').addEventListener('submit', function(event) {
     event.preventDefault();
 
     let users = JSON.parse(localStorage.getItem('users')) || [];
@@ -6,7 +6,10 @@ document.getElementById('registerForm').addEventListener('submit', function(even
     let lastname = document.getElementById('lastname').value;
     let username = document.getElementById('username').value;
     let email = document.getElementById('email').value;
+    let birthdate = document.getElementById('birthdate').value;
+    console.log(birthdate);
     let password = document.getElementById('password').value;
+    console.log(password);
 
     let exists = users.some(user => user.username === username || user.email === email);
 
@@ -18,3 +21,22 @@ document.getElementById('registerForm').addEventListener('submit', function(even
         document.getElementById('message').textContent = 'Usuario registrado exitosamente.';
     }
 });
+
+window.onload = function(){
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+
+    // Resta 18 años para la fecha mínima (mayor de edad)
+    var minYear = yyyy - 18;
+    var minDate = minYear + '-' + mm + '-' + dd;
+
+    // Resta 70 años para la fecha máxima
+    var maxYear = yyyy - 70;
+    var maxDate = maxYear + '-' + mm + '-' + dd;
+
+    // Establece las fechas mínima y máxima
+    document.getElementById('birthdate').setAttribute('max', minDate);
+    document.getElementById('birthdate').setAttribute('min', maxDate);
+}
